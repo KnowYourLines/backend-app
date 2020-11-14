18f1a2154967e7510854a3eb0aa8f3a68bbe23a5
Johnny Li <johnkcli@hotmail.com>
2020-11-14 16:59:22 +0000

    created models

--- src/lines/models.py
+++ src/lines/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Script(models.Model):
+    script_name = models.CharField(max_length=100)
+    writer = models.CharField(max_length=100)
+
+
+class Line(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    script = models.ForeignKey(Script, related_name='lines', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    cue = models.TextField()
+    id = models.CharField(max_length=36)
+    shouldPlay = models.BooleanField()
+
+    class Meta:
+        ordering = ["created"]
