baaf582b1ee03c64f3684bce5cadbb26646e1ddb
Johnny <knowyourlines@outlook.com>
2021-11-01 22:01:23 +0000

    try to make heroku happy

--- server/settings.py
+++ server/settings.py
@@ -33,17 +33,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
+    "rest_framework",
+    "rest_framework.authtoken",
+    "drf_registration",
+    "lines.apps.LinesConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "corsheaders",
-    "rest_framework",
-    "rest_framework.authtoken",
-    "drf_registration",
-    "lines.apps.LinesConfig",
 ]
 
 MIDDLEWARE = [
